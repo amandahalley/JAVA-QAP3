63355a03a813fc2d51f0f0a068816c55d3ef7f6c
ahllyy <amanda.halley@keyin.com>
2024-11-15 15:00:48 -0330

    fixed spelling error for Gender constructor

--- Problem1/Person.java
+++ Problem1/Person.java
@@ -5,11 +5,11 @@ public class Person {
     //Attributes
     protected String myName; 
     protected int myAge;
-    protected String myGener; 
+    protected String myGender; 
 
 
     //Constructor 
-    public Person(String name, int age, String Gender) {
+    public Person(String name, int age, String gender) {
         myName = name;
         myAge = age;
         myGender = gender;

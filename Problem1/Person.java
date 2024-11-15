3d326de9b362d4c241781b87ca875d1204d49a18
ahllyy <amanda.halley@keyin.com>
2024-11-15 15:03:32 -0330

    set up getters and setters base off constructor

--- Problem1/Person.java
+++ Problem1/Person.java
@@ -17,9 +17,30 @@ public class Person {
 
 
     //Getters 
+    public String getNmae() {
+        return myName;
+    }
 
+    public int getAge() {
+        return myAge;
+    }
+
+    public String getGender() {
+        return myGender;
+    }
 
     //Setters
+    public void setName(String name) {
+        myName = name;
+    }
+
+    public void setAge(int age) {
+        myAge = age;
+    }
+
+    public void setGender(String gender) {
+        myGender = gender;
+    }
 
 
     //toString method for print format

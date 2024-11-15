873bbee0821a134b8369a2ea39685cf4c3c9bd32
ahllyy <amanda.halley@keyin.com>
2024-11-15 16:12:49 -0330

    testing for Teacher class

--- Problem1/Demo.java
+++ Problem1/Demo.java
@@ -10,6 +10,9 @@ public class Demo {
         //Test for Student Class
         Student carson = new Student("Carson Thorne", 30, "M", "J283789", 3.5);
         System.out.println(carson);
+
+        Teacher george = new Teacher("Mr. George", 32, "M", "Computer Science", 50000);
+        System.out.println(george);
     }
 
 }

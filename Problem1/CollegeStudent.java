0a9329294ace3a4df817357d67d7963bcf142463
ahllyy <amanda.halley@keyin.com>
2024-11-15 16:24:00 -0330

    Added instance variables, created constructor using super to reference constructor in Student class, initialized new info

--- Problem1/CollegeStudent.java
+++ Problem1/CollegeStudent.java
@@ -1,5 +1,18 @@
 package Problem1;
 
-public class CollegeStudent {
-    
+public class CollegeStudent extends Student {
+    protected String myMajor; //Major of College Student
+    protected int myYear; //Current year in attendance (FROSH = 1,  SOPH = 2, …)
+
+    public CollegeStudent(String name, int age, String gender, String idNum, int GPA, String major, int year) {
+        //super class constructor (calling Student)
+        super(name, age, gender, idNum, GPA); 
+
+        //Initialize new info to CollegeStudent
+        myMajor = major;
+        myYear = year;
+        
+    }
+
+
 }

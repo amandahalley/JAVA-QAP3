1dddc4b06798ee709ecb81c8d7085d5fff6be11e
ahllyy <amanda.halley@keyin.com>
2024-11-15 16:13:05 -0330

    added getters, setters and toString method

--- Problem1/Teacher.java
+++ Problem1/Teacher.java
@@ -13,14 +13,33 @@ public Teacher (String name, int age, String gender, String subject, double sala
     mySubject = subject;
     mySalary = salary;
 
+}
     //Getters
-    
+    public String getSubject() {
+        return mySubject;
+    }
 
+    public double getSalary() {
+        return mySalary;
+    }
 
     //Setters
+    public void setSubject(String subject) {
+        mySubject = subject;
+    }
+
+    public void setSalary(double salary) {
+        mySalary = salary;
+    }
+
+    //Print format
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Salary: $" + mySalary + ", Subject: " + mySubject;
+    }
 
 
-}
 
     
 }

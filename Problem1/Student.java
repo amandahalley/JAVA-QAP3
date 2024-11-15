3d645463eecaf6c0e6f3a810c9b1a1adf94c5e1d
ahllyy <amanda.halley@keyin.com>
2024-11-15 15:13:40 -0330

    added given code for student

--- Problem1/Student.java
+++ Problem1/Student.java
@@ -1,5 +1,15 @@
 package Problem1;
 
-public class Student {
+class Student extends Person {
+    protected String myIdNum;    // Student Id Number
+    protected double myGPA;      // grade point average
+
+    public Student(String name, int age, String gender, String idNum, double gpa)  {
+        // use the super class’ constructor
+        super(name, age, gender);
+        // initialize what’s new to Student
+        myIdNum = idNum;
+        myGPA = gpa;
+    }    
+ }
     
-}

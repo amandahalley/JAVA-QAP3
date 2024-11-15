297ef143a39191360e25fb339434baab4bec11a7
ahllyy <amanda.halley@keyin.com>
2024-11-15 14:59:33 -0330

    Start person class, added attributes and the constructor - comments for what else needs to be done

---
+++ Problem1/Person.java
@@ -0,0 +1,26 @@
+package Problem1; 
+
+public class Person {
+
+    //Attributes
+    protected String myName; 
+    protected int myAge;
+    protected String myGener; 
+
+
+    //Constructor 
+    public Person(String name, int age, String Gender) {
+        myName = name;
+        myAge = age;
+        myGender = gender;
+    }
+
+
+    //Getters 
+
+
+    //Setters
+
+
+    //toString method for print format
+}

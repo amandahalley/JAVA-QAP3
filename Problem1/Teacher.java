40f5b5ac5b339e369c0af8fafd484a5125c82f63
ahllyy <amanda.halley@keyin.com>
2024-11-15 15:45:43 -0330

    created instance variabls for teacher and created constructor using super to reference constructor from person class

--- Problem1/Teacher.java
+++ Problem1/Teacher.java
@@ -1,5 +1,26 @@
 package Problem1;
 
-public class Teacher {
+public class Teacher extends Person {
+
+protected String mySubject; //Subject taught
+protected double mySalary; //Teachers salary
+
+public Teacher (String name, int age, String gender, String subject, double salary) {
+    //super class constructor
+    super(name, age, gender);
+
+    //Initialize what is new to Teacher
+    mySubject = subject;
+    mySalary = salary;
+
+    //Getters
+    
+
+
+    //Setters
+
+
+}
+
     
 }

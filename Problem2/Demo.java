bb4930bd1da1f70cc878fb6a174eff593fa3783b
ahllyy <amanda.halley@keyin.com>
2024-11-19 17:39:02 -0330

    testing for moveable point

--- Problem2/Demo.java
+++ Problem2/Demo.java
@@ -9,5 +9,16 @@ public class Demo {
 
         p1.setXY(4.5f, 5.5f);
         System.out.println("Updated Point: " + p1);
+
+        //Test for MovablePoint class
+        MovablePoint mpoint1 = new MovablePoint(1.0f,2.0f,0.3f,0.4f);
+        System.out.println("MovablePoint:" + mpoint1);
+
+        mpoint1.move();
+        System.out.println("Point after move: " + mpoint1 );
+
+        mpoint1.setSpeed(1.0f, 1.3f);
+        mpoint1.move();
+        System.out.println("Point after move 2: " + mpoint1);
     }
 }

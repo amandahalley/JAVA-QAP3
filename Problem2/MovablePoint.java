575a0aa6045a596e751c207afd135a277e01f99b
ahllyy <amanda.halley@keyin.com>
2024-11-19 17:16:19 -0330

    set up constructors, using super to call parent class for details

--- Problem2/MovablePoint.java
+++ Problem2/MovablePoint.java
@@ -1,5 +1,33 @@
 package Problem2;
 
-public class MovablePoint {
-    
+public class MovablePoint extends Point {
+    private float xSpeed;
+    private float ySpeed;
+
+    //parameterized constructor 
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+    //No-argument constructor
+    public MovablePoint() {
+        xSpeed = 0.0f;
+        ySpeed = 0.0f;
+    }
+
+    //parameterized constructor to initialize movablepoint with specifc speed and coordinates
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x,y); //calling point class (parent) to set x & y
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+
+    //Getters
+
+
+    //Setters
+
+
+    //toString
 }

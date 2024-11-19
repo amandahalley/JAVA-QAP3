e19c1e66e0d69ac7faed7353f665b4a61275284f
ahllyy <amanda.halley@keyin.com>
2024-11-19 16:25:26 -0330

    implemented parameterized constructor & no argument constructor

---
+++ Problem2/Point.java
@@ -0,0 +1,20 @@
+package Problem2;
+
+public class Point {
+    private float x; //x coordinate
+    private float y; //y coordinate
+
+    //Parameterized constructor 
+    public Point(float x, float y) {
+    this.x = x;
+    this.y = y;
+    }
+
+    //No-argument constructor
+    public Point() {
+        x = 0.0f;
+        y = 0.0f;
+    }
+
+    
+}

249838b29d37bd18e5f9688d648e842bb901e401
ahllyy <amanda.halley@keyin.com>
2024-11-19 17:34:46 -0330

    Testing for Point class

--- Problem2/Demo.java
+++ Problem2/Demo.java
@@ -1,5 +1,13 @@
 package Problem2;
 
 public class Demo {
-    
+    public static void main(String[] args) {
+        
+        //Test for Point class
+        Point p1 = new Point(2.3f, 3.4f);
+        System.out.println("Point: " + p1);
+
+        p1.setXY(4.5f, 5.5f);
+        System.out.println("Updated Point: " + p1);
+    }
 }

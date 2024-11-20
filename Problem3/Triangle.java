7e7e1774829445b6f729851f4d66944089a0be94
ahllyy <amanda.halley@keyin.com>
2024-11-20 18:34:55 -0330

    basic outline for traingle class

--- Problem3/Triangle.java
+++ Problem3/Triangle.java
@@ -1,5 +1,22 @@
 package Problem3;
 
-public class Triangle {
+public class Triangle extends Shape {
+    private double side1;
+    private double side2;
+    private double side3;
     
+
+    public Triangle(doubel side1, double side2, double side3) {
+
+    }
+
+    @Override
+    public double getPerimeter(){
+
+    }
+
+    @Override
+    public double getArea(){
+        
+    }
 }

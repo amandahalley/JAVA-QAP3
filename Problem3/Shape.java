40f006497c6eac33dc0d8b6d395938eaee020f61
ahllyy <amanda.halley@keyin.com>
2024-11-20 17:45:24 -0330

    added methods to get area and perimeter of shape, as well as toString method

--- Problem3/Shape.java
+++ Problem3/Shape.java
@@ -8,8 +8,15 @@ public abstract class Shape {
         this.name = name;
     }
 
-    //Abstract methods for calculating are and perimeter of shapes
+    //Abstract methods for calculating area and perimeter of shapes
+    //These methods should be used by any subclass of this Shape class
+    public abstract double getPerimeter();
 
+    public abstract double getArea();
 
-    //toString method
+    //toString method to give representation of the shape
+    @Override
+    public String toString() {
+        return "Shape: " + name + "Perimeter:" +getPerimeter() + "Area: " + getArea();
+    }
 }

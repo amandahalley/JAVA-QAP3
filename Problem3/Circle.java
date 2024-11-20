f444bfa5cfbd1e9197cd7aea912d8f3d7f8d7ef4
ahllyy <amanda.halley@keyin.com>
2024-11-20 17:55:18 -0330

    Created constructor to set name of shape and initialize the radius, implemented formulas to calculate the perimeter and area of a circle

--- Problem3/Circle.java
+++ Problem3/Circle.java
@@ -1,5 +1,26 @@
 package Problem3;
 
-public class Circle {
-    
+public class Circle extends Shape {
+    private double radius;
+
+
+    //
+    public Circle(double radius) {
+        // call constructor of Shape (super class) to set the name to circle
+        super("Circle");
+        //assign radius to instance variable
+        this.radius = radius;
+    }
+
+    //implements formula to calculate perimeter of a circle
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    //implements formula to caluclate the area of a circle
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
 }

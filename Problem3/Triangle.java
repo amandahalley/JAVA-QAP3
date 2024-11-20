7631a57f16a747261d627ee9257cd079d28c64fd
ahllyy <amanda.halley@keyin.com>
2024-11-20 18:58:38 -0330

    fixed error in validation logic

--- Problem3/Triangle.java
+++ Problem3/Triangle.java
@@ -8,10 +8,10 @@ public class Triangle extends Shape {
     //Constructor
     public Triangle(double side1, double side2, double side3) {
         //Call superclass constructor to asssign name of shape
-        super("Trianlge");
+        super("Triangle");
         //validate side input meets triangle inequality
         // sum of any 2 sides must be greater then the third side
-        if (side1 + side2 > side3 || side2 + side3 > side1 || side3 + side1 > side2) {
+        if (side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side2){
             this.side1 = side1;
             this.side2 = side2;
             this.side3 = side3;
@@ -32,4 +32,5 @@ public class Triangle extends Shape {
         double s = getPerimeter() / 2;
         return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
     }
+
 }

e52874c0bfa4465469f8caaa995e45ff925308fb
ahllyy <amanda.halley@keyin.com>
2024-11-20 18:32:12 -0330

    Test for ellipse

--- Problem3/Demo.java
+++ Problem3/Demo.java
@@ -4,9 +4,12 @@ public class Demo {
     public static void main(String[] args) {
         
     //Testing classes
-    //Circle class
     Circle circle = new Circle(5.0);
     System.out.println(circle);
+
+    Ellipse ellipse = new Ellipse(3, 4);
+    System.out.println(ellipse);
+    
     
 
     }

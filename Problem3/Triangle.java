72c8332a269e634b63f83fdd48d2ccfc7d2256ec
ahllyy <amanda.halley@keyin.com>
2024-11-20 18:42:41 -0330

    set up constructor with validation of the 3 sides of the triangle

--- Problem3/Triangle.java
+++ Problem3/Triangle.java
@@ -5,18 +5,29 @@ public class Triangle extends Shape {
     private double side2;
     private double side3;
     
-
-    public Triangle(doubel side1, double side2, double side3) {
-
+    //Constructor
+    public Triangle(double side1, double side2, double side3) {
+        //Call superclass constructor to asssign name of shape
+        super("Trianlge");
+        //validate side input meets triangle inequality
+        // sum of any 2 sides must be greater then the third side
+        if (side1 + side2 > side3 || side2 + side3 > side1 || side3 + side1 > side2) {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        } else {
+            //Error if sides are invalid
+            throw new IllegalArgumentException("Sides of triangle are invalid.");
+        }
     }
 
     @Override
     public double getPerimeter(){
-
+        
     }
 
     @Override
     public double getArea(){
-        
+
     }
 }

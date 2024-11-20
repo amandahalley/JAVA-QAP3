4b9fd2854335b292e4b9c53f163d2447cb148901
ahllyy <amanda.halley@keyin.com>
2024-11-20 18:06:47 -0330

    Testing for circle

--- Problem3/Demo.java
+++ Problem3/Demo.java
@@ -1,5 +1,13 @@
 package Problem3;
 
 public class Demo {
+    public static void main(String[] args) {
+        
+    //Testing classes
+    //Circle class
+    Circle circle = new Circle(5.0);
+    System.out.println(circle);
     
+
+    }
 }

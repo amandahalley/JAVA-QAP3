5f4daf58fe54b20e9288b45169d4b1a922b0b84c
ahllyy <amanda.halley@keyin.com>
2024-11-20 17:41:02 -0330

    Created all required classes

---
+++ Problem3/Shape.java
@@ -0,0 +1,15 @@
+package Problem3;
+
+public abstract class Shape {
+    protected String name;
+
+    //Constructor, initializing shapes name
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    //Abstract methods for calculating are and perimeter of shapes
+
+
+    //toString method
+}

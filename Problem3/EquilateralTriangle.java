013171244fae7f7fa2e119ac5ad37af32e94cb5b
ahllyy <amanda.halley@keyin.com>
2024-11-21 15:44:51 -0330

    complete class

--- Problem3/EquilateralTriangle.java
+++ Problem3/EquilateralTriangle.java
@@ -2,7 +2,7 @@ package Problem3;
 
 public class EquilateralTriangle extends Triangle {
     public EquilateralTriangle(double side) {
-        super(side, side, side); //call constructor of superclass (triangle) to obtain sides
+        super(side, side, side); //call constructor of superclass (triangle) to obtain side
         name = "Equilateral Triangle"; //asign name to shape
     }
 }

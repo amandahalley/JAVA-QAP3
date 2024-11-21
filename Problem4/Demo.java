001aa391faf7eb43ec534e903b8ab8773f2b9e29
ahllyy <amanda.halley@keyin.com>
2024-11-21 16:37:56 -0330

    created testing for scaling, printin shapes before and after

--- Problem4/Demo.java
+++ Problem4/Demo.java
@@ -1,5 +1,32 @@
 package Problem4;
 
 public class Demo {
-    
+    public static void scaleShapes(Scalable[] shapes, double factor) {
+        System.out.println("Shapes before scaling:");
+        for (Scalable shape : shapes) {
+            System.out.println(shape);
+        }
+
+        for (Scalable shape : shapes) {
+            shape.scale(factor);
+        }
+
+        System.out.println("Shapes after scaling:");
+        for (Scalable shape : shapes) {
+            System.out.println(shape);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Create an array of shapes implementing Scalable
+        Scalable[] shapes = {
+            new Circle(10),
+            new Ellipse(8,5),
+            new Triangle(2,3,4),
+            new EquilateralTriangle(4)
+        };
+
+        // Test scaling with a factor of 2
+        scaleShapes(shapes, 2);
+    }
 }

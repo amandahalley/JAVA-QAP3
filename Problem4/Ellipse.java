e8aba0e0e1da74bb9726fc674c198d0543522740
ahllyy <amanda.halley@keyin.com>
2024-11-21 16:15:27 -0330

    added scale method

--- Problem4/Ellipse.java
+++ Problem4/Ellipse.java
@@ -1,5 +1,39 @@
 package Problem4;
 
-public class Ellipse {
-    
+public class Ellipse extends Shape {
+    private double majorAxis;
+    private double minorAxis;
+
+    //Constructor
+    public Ellipse(double a, double b) {
+        // call superclass constructor to set the name
+        super("Ellipse");
+        //To assign largest value to majorAxis always
+        if (a > b) {
+            this.majorAxis = a;
+            this.minorAxis = b;
+        } else {
+            this.majorAxis = b;
+            this.minorAxis = a;
+        }
+    }
+
+    //Formula to calculate perimeter
+    @Override
+    public double getPerimeter() {
+        return Math.PI * 2 * (Math.pow(majorAxis, 2) + Math.pow(minorAxis, 2) - Math.pow(majorAxis - minorAxis, 2) / 2);
+    }
+
+    //Formula to calculate area 
+    @Override
+    public double getArea() {
+        return Math.PI * majorAxis * minorAxis;
+    }
+
+    //implements scale method
+    @Override
+    public void scale(double factor) {
+        majorAxis *= factor;
+        minorAxis *= factor;
+    }
 }

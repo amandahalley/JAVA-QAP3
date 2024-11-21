923a1d53321986514a07da53e675d1ed39f1e643
ahllyy <amanda.halley@keyin.com>
2024-11-21 18:18:46 -0330

    clean up

--- Problem1/Demo.java
+++ Problem1/Demo.java
@@ -3,11 +3,9 @@ package Problem1;
 public class Demo {
     public static void main(String[] args) {
 
-        //Test for Person Class
         Person amanda = new Person("Amanda Halley", 26, "F");
         System.out.println(amanda);
 
-        //Test for Student Class
         Student carson = new Student("Carson Thorne", 30, "M", "J283789", 3.5);
         System.out.println(carson);
 
